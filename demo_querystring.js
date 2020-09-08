b35428f3242493a0352bd072d9c8361db0a5f6f6
Erin Borders <borders16@gmail.com>
2020-09-08 15:31:59 -0400

    more in depth url parsing example

--- demo_querystring.js
+++ demo_querystring.js
@@ -9,3 +9,16 @@ http.createServer(function (req, res) {
     var text = q.year + " " + q.month;
     res.end(text);
 }).listen(8080);
+
+// parse an address with url.parse() and it'll return a URL object
+// with each part of the address as properties:
+
+var address = 'http://localhost:8080/default.htm?year=2017&month=february';
+var q = url.parse(address, true);
+
+console.log(q.host); // returns 'localhost:8080
+console.log(q.pathname); // returns '/default.htm'
+console.log(q.search); // returns '?year=2017&month=february'
+
+var qdata = q.query; // returns { year: 2017, month: 'february'}
+console.log(qdata.month); // returns 'february'
