b8eb9cdf5a98deeffdd5040a0c93db0fc73b7b9b
Erin Borders <borders16@gmail.com>
2020-09-09 20:40:52 -0400

    created route handlers

--- assignments/basic-express-site/app.js
+++ assignments/basic-express-site/app.js
@@ -1,29 +1,34 @@
 var express = require('express')
+var path = require('path')
 var app = express()
 
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+// })
+
+app.use(express.static(path.join(__dirname, 'files')))
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.sendFile('files/index.html')
 })
 
-// app.get('/', (req, res) => {
-//     res.sendFile('index.html')
-// })
+app.get('/index', (req, res) => {
+    res.sendFile(path.join(__dirname, 'files/index.html'))
+})
 
-// app.get('/index', (req, res) => {
-//     res.sendFile('index.html')
-// })
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'files/about.html'))
+})
 
-// app.get('/about', (req, res) => {
-//     res.sendFile('about.html')
-// })
+app.get('/contactme', (req, res) => {
+    res.sendFile(path.join(__dirname, 'files/contact-me.html'))
+})
 
-// app.get('/contactme', (req, res) => {
-//     res.sendFile('contact-me.html')
-// })
-
-// app.use(function (err, req, res, next) {
-//     res.sendFile('404.html')
-// })
+app.use('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/files/404.html'))
+    // have to put another / otherwise there's no /between root of site
+    // and prefix
+})
 
 app.listen(8000, () => {
     console.log('Basic express app listening on port 8000')
