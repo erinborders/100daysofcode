c2fcf8de1a23f3715adfd2cb207dcf836d2c933d
Erin Borders <borders16@gmail.com>
2020-09-09 20:12:02 -0400

    verify express is working

--- assignments/basic-express-site/app.js
+++ assignments/basic-express-site/app.js
@@ -1,3 +1,30 @@
 var express = require('express')
 var app = express()
 
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+// app.get('/', (req, res) => {
+//     res.sendFile('index.html')
+// })
+
+// app.get('/index', (req, res) => {
+//     res.sendFile('index.html')
+// })
+
+// app.get('/about', (req, res) => {
+//     res.sendFile('about.html')
+// })
+
+// app.get('/contactme', (req, res) => {
+//     res.sendFile('contact-me.html')
+// })
+
+// app.use(function (err, req, res, next) {
+//     res.sendFile('404.html')
+// })
+
+app.listen(8000, () => {
+    console.log('Basic express app listening on port 8000')
+})
