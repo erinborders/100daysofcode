62fdc6aa7b7018a8b6291a4bf100f98aee6a1ae3
Erin Borders <borders16@gmail.com>
2020-09-11 15:23:10 -0400

    mongoose and CRUD actions with models

--- notes/mongoose-primer.js
+++ notes/mongoose-primer.js
@@ -34,6 +34,7 @@ var modelSchema = new Schema({
 })
 
 // CREATING A MODEL
+// models represent a collection of documents and documents are single instances of the model
 // compile model from above schema with the name of the collection and the schema you want to use
 var SomeModel = mongoose.model('SomeModel', mongoose.modelSchema);
 
@@ -61,3 +62,76 @@ var schema = new Schema(
         }
     }
 )
+
+// VALIDATION
+
+// mongoose provides built in and custom validators, and sync and async validators
+// all schema types have the required validator
+// numbers have the min and max validators
+// strings have enum (specifies the set of allowed values for the field), match (specifies
+// a regular expression that the string must match), maxlength and minlength
+
+// EXAMPLE
+var breakfastSchema = new Schema({
+    eggs: {
+      type: Number,
+      min: [6, 'Too few eggs'],
+      max: 12,
+      required: [true, 'Why no eggs?']
+    },
+    drink: {
+      type: String,
+      enum: ['Coffee', 'Tea', 'Water',]
+    }
+  });
+
+  // VIRTUAL PROPERTIES
+  // document properties that you can get and set but that don't get persisted to 
+  // mongodb
+
+  // EXAMPLE
+   // define a schema
+   var personSchema = new Schema({
+    name: {
+      first: String,
+      last: String
+    }
+  });
+
+  // compile our model
+  var Person = mongoose.model('Person', personSchema);
+
+  // create a document
+  var axl = new Person({
+    name: { first: 'Axl', last: 'Rose' }
+  });
+
+  // becomes this with a virtual property:
+  personSchema.virtual('fullName').get(function () {
+    return this.name.first + ' ' + this.name.last;
+  });
+
+  // methods and query helpers
+  // a schema can have instance methods (methods you put on the schema that you can then
+  // call from any of the models and instances created off them), static methods (regular
+  // methods on a particular model) and query helpers (what we were using in the bootcamp)
+
+  // creating and modifying documents
+  // to create a record you can define an instance of the model and then call save()
+var awesome_instance = new SomeModel ({ name: 'awesome'})
+awesome_instance.save(function (err) {
+    if (err) return handleError(err)
+    //saved!
+})
+
+// creation of records (along with updates, deletes and queries) are async
+// the first argumnet for the callback will always be an error value
+// you can also use create() to create the instance at the same time you save it
+
+SomeModel.create({ name: 'also awesome' }, function (err, awesome_instance) {
+    if (err) return handleError(err)
+})
+
+// Every model has an associated connection (this will be the default connection when 
+// you use mongoose.model()). You create a new connection and call .model() on it to 
+// create the documents on a different database.
