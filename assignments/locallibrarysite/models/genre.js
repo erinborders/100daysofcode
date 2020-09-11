c879e30d29574067e0fa56811f5314a8fbff0edb
Erin Borders <borders16@gmail.com>
2020-09-11 16:48:30 -0400

    created genre schema and model

---
+++ assignments/locallibrarysite/models/genre.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose')
+
+var Schema = mongoose.Schema;
+
+var GenreSchema = new Schema(
+    {
+        name: { type: String, required: true, minlength: 3, maxlength: 100},
+    }
+)
+
+// virtual property for url
+GenreSchema
+.virtual('url')
+.get(function () {
+    return '/catalog/genre/' + this._id
+})
+
+module.exports = mongoose.Model('Genre', GenreSchema)
