b5f19c8b003f20156abcbc34b8faebfc492ac9cf
Erin Borders <borders16@gmail.com>
2020-09-11 17:37:10 -0400

    created sample messages

--- assignments/mini-message-board/routes/index.js
+++ assignments/mini-message-board/routes/index.js
@@ -1,6 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "I'm the first message!",
+    user: "Erin",
+    added: new Date()
+  },
+  {
+    text: "I'm the second message",
+    user: "Loser",
+    added: new Date()
+  }
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
