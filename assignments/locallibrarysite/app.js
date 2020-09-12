dced67f636a631b7b669214e27e4faf20580a0e8
Erin Borders <borders16@gmail.com>
2020-09-12 11:30:21 -0400

    added router and route

--- assignments/locallibrarysite/app.js
+++ assignments/locallibrarysite/app.js
@@ -11,6 +11,7 @@ var logger = require('morgan');
 // contain the code for handling particular sets of related routes (url paths)
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var catalogRouter = require('./routes/catalog')
 
 var app = express();
 
@@ -35,6 +36,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // adding our route handling code to the request handling chain
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
