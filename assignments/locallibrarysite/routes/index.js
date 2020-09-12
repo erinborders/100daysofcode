c3bdda76037d9ba58576207b7f1cd7f2bc7db1f3
Erin Borders <borders16@gmail.com>
2020-09-12 11:21:48 -0400

    created routes to model methods in controllers

--- assignments/locallibrarysite/routes/index.js
+++ assignments/locallibrarysite/routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.redirect('/catalog')
+  // sends 302 'found' status code by default
 });
 
 module.exports = router;
