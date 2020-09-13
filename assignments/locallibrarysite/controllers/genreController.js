27faa4d393b23840f923e9c8562bc3f424890903
Erin Borders <borders16@gmail.com>
2020-09-12 23:29:59 -0400

    populate genre details

--- assignments/locallibrarysite/controllers/genreController.js
+++ assignments/locallibrarysite/controllers/genreController.js
@@ -1,6 +1,8 @@
 var Genre = require('../models/genre');
 const { nextTick } = require('async');
 const genre = require('../models/genre');
+var Book = require('../models/book');
+var async = require('async');
 
 // Display list of all Genre.
 exports.genre_list = function(req, res) {
@@ -13,7 +15,27 @@ exports.genre_list = function(req, res) {
 
 // Display detail page for a specific Genre.
 exports.genre_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre detail: ' + req.params.id);
+    async.parallel({
+        genre: function(callback) {
+            Genre.findById(req.params.id)
+              .exec(callback);
+        },
+
+        genre_books: function(callback) {
+            Book.find({ 'genre': req.params.id })
+              .exec(callback);
+        },
+
+    }, function(err, results) {
+        if (err) { return next(err); }
+        if (results.genre==null) { // No results.
+            var err = new Error('Genre not found');
+            err.status = 404;
+            return next(err);
+        }
+        // Successful, so render
+        res.render('genre_detail', { title: 'Genre Detail', genre: results.genre, genre_books: results.genre_books } );
+    });
 };
 
 // Display Genre create form on GET.
